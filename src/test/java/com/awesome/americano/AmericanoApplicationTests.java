71835415c1f6c8fe5d5b3c090a6b068bca40a2b3
dajeongdev <dajeongdev@gmail.com>
2024-03-21 14:22:07 +0900

    Initial commit

---
+++ src/test/java/com/awesome/americano/AmericanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awesome.americano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmericanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
