ef60f9981aa6a073e3d13652d5bc2f5337992573
dajeongdev <dajeongdev@gmail.com>
2024-03-23 18:08:47 +0900

    feat : 스웨거 설정 확인을 위한 간단한 코드 추가

--- src/main/java/com/coffee/americanote/user/controller/UserController.java
+++ src/main/java/com/coffee/americanote/user/controller/UserController.java
@@ -2,11 +2,13 @@ package com.coffee.americanote.user.controller;
 
 import com.coffee.americanote.user.User;
 import com.coffee.americanote.user.UserRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "User", description = "사용자 관련 API입니다.")
 @RequiredArgsConstructor
 @RestController
 public class UserController {
