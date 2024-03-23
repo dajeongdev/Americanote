baa121deed2b2aae4b0001bcb57e7a623835a3d2
dajeongdev <dajeongdev@gmail.com>
2024-03-23 16:40:37 +0900

    refact : 패키지명 변경

--- src/main/java/com/coffee/americanote/AmericanoApplication.java
+++ src/main/java/com/coffee/americanote/AmericanoApplication.java
@@ -1,4 +1,4 @@
-package com.awesome.americano;
+package com.coffee.americanote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/coffee/americanote/AmericanoApplicationTests.java
+++ src/test/java/com/coffee/americanote/AmericanoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.awesome.americano;
+package com.coffee.americanote;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
