b7b16dee9bbb1fb4573e3a17e107075f9696d769
dabeann <dabin4078@daum.net>
2024-03-24 17:22:54 +0900

    test: api test

--- src/test/java/com/coffee/americanote/api/ApiTest.java
+++ src/test/java/com/coffee/americanote/api/ApiTest.java
@@ -1,2 +1,29 @@
-package com.coffee.americanote.api;public class ApiTest {
+package com.coffee.americanote.api;
+
+import com.coffee.americanote.navermap.AddressToCoordinate;
+import com.coffee.americanote.navermap.CrawlingCafe;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiTest {
+
+    @Autowired
+    private CrawlingCafe crawlingCafe;
+    @Autowired
+    private AddressToCoordinate addressToCoordinate;
+
+    @Test
+    public void ctest1() throws InterruptedException {
+        crawlingCafe.process();
+        //System.out.println(crawlingCafe.coffeeInfoms.size());
+    }
+
+    @Test
+    public void ctest2() {
+        String[] result = addressToCoordinate.addressToCoordinate("서울 마포구 월드컵북로 48-3 1층");
+        System.out.println("result[0] = " + result[0]);
+        System.out.println("result[1] = " + result[1]);
+    }
 }
