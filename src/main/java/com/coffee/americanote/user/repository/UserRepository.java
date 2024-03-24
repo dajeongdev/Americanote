7b530fd6f5dcadd453dd13044e8f0c72798ebd77
dajeongdev <dajeongdev@gmail.com>
2024-03-24 17:29:30 +0900

    refactor : 불필요한 import와 접근제한자 수정

--- src/main/java/com/coffee/americanote/user/repository/UserRepository.java
+++ src/main/java/com/coffee/americanote/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
-package com.coffee.americanote.user;
+package com.coffee.americanote.user.repository;
 
+import com.coffee.americanote.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
