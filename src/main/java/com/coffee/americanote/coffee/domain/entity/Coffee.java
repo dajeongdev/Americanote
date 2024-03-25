360321a014914e6e21ce72e5850c173d158b39e7
dabeann <dabin4078@daum.net>
2024-03-25 17:55:52 +0900

    feat: coffee flavour mapping

--- src/main/java/com/coffee/americanote/coffee/domain/entity/Coffee.java
+++ src/main/java/com/coffee/americanote/coffee/domain/entity/Coffee.java
@@ -13,7 +13,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -49,4 +52,7 @@ public class Coffee extends BaseEntity {
 
     @Column(nullable = false)
     private int price;
+
+    @OneToMany(mappedBy = "coffee")
+    private List<CoffeeFlavour> flavours = new ArrayList<>();
 }
