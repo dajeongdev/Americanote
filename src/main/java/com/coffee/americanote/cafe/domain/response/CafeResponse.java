3aa6c50830dfbaa717289d66af1986ada95e938a
dabeann <dabin4078@daum.net>
2024-03-26 13:53:18 +0900

    feat: get all cafe api

--- src/main/java/com/coffee/americanote/cafe/domain/response/CafeResponse.java
+++ src/main/java/com/coffee/americanote/cafe/domain/response/CafeResponse.java
@@ -4,20 +4,14 @@ import com.coffee.americanote.cafe.domain.entity.Cafe;
 
 public record CafeResponse(
         Long id,
-        String name,
-        String address,
         double latitude,
-        double longitude,
-        String imageUrl
+        double longitude
 ) {
     public CafeResponse(Cafe cafe) {
         this(
                 cafe.getId(),
-                cafe.getName(),
-                cafe.getAddress(),
                 cafe.getLatitude(),
-                cafe.getLongitude(),
-                cafe.getImageUrl()
+                cafe.getLongitude()
         );
     }
 }
