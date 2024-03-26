87130030830982a06e4577b52e2fcc18cbfe7477
dajeongdev <dajeongdev@gmail.com>
2024-03-27 00:35:30 +0900

    feat : 카카오 로그인에 필요한 필드 추가

--- src/main/java/com/coffee/americanote/user/domain/entity/User.java
+++ src/main/java/com/coffee/americanote/user/domain/entity/User.java
@@ -1,7 +1,6 @@
 package com.coffee.americanote.user.domain.entity;
 
 import com.coffee.americanote.common.entity.BaseEntity;
-import com.coffee.americanote.user.domain.request.UserRequest;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,12 +19,12 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
-    private String name;
+    @Column(name = "kakao_id")
+    private Long kakaoId;
 
-    public static User toUserEntity(UserRequest userRequest) {
-        return User.builder()
-                .name(userRequest.name())
-                .build();
-    }
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
 }
