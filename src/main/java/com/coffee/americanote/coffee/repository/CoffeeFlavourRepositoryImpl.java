2bbe5b1e6fae52739d9c8e27e8d7071412376aff
dabeann <dabin4078@daum.net>
2024-03-28 23:17:13 +0900

    feat: flavour max num

--- src/main/java/com/coffee/americanote/coffee/repository/CoffeeFlavourRepositoryImpl.java
+++ src/main/java/com/coffee/americanote/coffee/repository/CoffeeFlavourRepositoryImpl.java
@@ -5,11 +5,14 @@ import static com.coffee.americanote.coffee.domain.entity.QCoffeeFlavour.*;
 import com.coffee.americanote.cafe.domain.request.SearchCafeRequest;
 import com.coffee.americanote.coffee.domain.entity.CoffeeFlavour;
 import com.coffee.americanote.common.entity.Flavour;
+import com.coffee.americanote.common.exception.CommonException;
+import com.coffee.americanote.common.exception.CommonExceptionHandler;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 
 public class CoffeeFlavourRepositoryImpl implements CoffeeFlavourRepositoryCustom {
 
@@ -30,6 +33,9 @@ public class CoffeeFlavourRepositoryImpl implements CoffeeFlavourRepositoryCusto
     private BooleanExpression inFlavour(List<String> flavours) {
         if (flavours == null || flavours.isEmpty()) {
             return coffeeFlavour.flavour.isNotNull();
+        }
+        if (flavours.size() > 3) {
+            throw new CommonException("허용된 개수 초과", HttpStatus.BAD_REQUEST);
         }
         List<Flavour> flavourEnums = flavours.stream()
                 .map(Flavour::valueOfLabel).toList();
