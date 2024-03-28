a6aa386556f117195a100e91206dd49511333154
dajeongdev <dajeongdev@gmail.com>
2024-03-28 14:39:10 +0900

    fix : header에서 accessToken 받는 형식 수정 - @RequestHeader -> HttpServletRequest

--- src/main/java/com/coffee/americanote/user/controller/UserController.java
+++ src/main/java/com/coffee/americanote/user/controller/UserController.java
@@ -10,12 +10,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Tag(name = "User", description = "사용자 관련 API입니다.")
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
@@ -29,11 +32,12 @@ public class UserController {
     @BasicApiSwaggerResponse
     @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json"))
     @GetMapping("/kakao")
-    ResponseEntity<Void> login(@RequestParam("code") String code,
-                               @RequestHeader(value = "Authorization", required = false) String accessToken) {
+    ResponseEntity<Void> login(@RequestParam("code") String code, HttpServletRequest request) {
+        String accessToken = request.getHeader("Authorization");
         if (accessToken == null) {
             accessToken = userService.getJwtToken(code);
         }
+        log.info("accessToken = {}", accessToken);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + accessToken);
         return new ResponseEntity<>(null, headers, HttpStatus.OK);
@@ -43,18 +47,16 @@ public class UserController {
     @BasicApiSwaggerResponse
     @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json"))
     @GetMapping("/mypage")
-    ResponseEntity<CommonResponse<UserResponse>> getMyData(
-            @RequestHeader(value = "Authorization") String accessToken) {
-        return new ResponseEntity<>(new CommonResponse<>("마이페이지", myPageService.getMyData(accessToken)), HttpStatus.OK);
+    ResponseEntity<CommonResponse<UserResponse>> getMyData(HttpServletRequest request) {
+        return new ResponseEntity<>(new CommonResponse<>("마이페이지", myPageService.getMyData(request.getHeader("Authorization"))), HttpStatus.OK);
     }
 
     @Operation(summary = "summary: 내 취향 커피 고르기", description = "description : return ok")
     @BasicApiSwaggerResponse
     @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json"))
     @PutMapping("/choose/prefer")
-    ResponseEntity<Void> updatePrefer(@RequestHeader(value = "Authorization") String accessToken,
-                                      @RequestBody UserPreferRequest userPreferRequest){
-        myPageService.updatePrefer(accessToken, userPreferRequest);
+    ResponseEntity<Void> updatePrefer(@RequestBody UserPreferRequest userPreferRequest, HttpServletRequest request){
+        myPageService.updatePrefer(request.getHeader("Authorization"), userPreferRequest);
         return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
