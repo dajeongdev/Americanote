8af9a9aa2064544b6c6dc30d8700d2d962f78f03
dabeann <dabin4078@daum.net>
2024-03-29 17:14:54 +0900

    feat: degree weight

--- src/main/java/com/coffee/americanote/common/entity/Degree.java
+++ src/main/java/com/coffee/americanote/common/entity/Degree.java
@@ -6,18 +6,24 @@ import java.util.stream.Stream;
 
 public enum Degree {
 
-    STRONG("강함"),
-    MEDIUM("중간"),
-    WEAK("약함");
+    STRONG("강함", 1.0),
+    MEDIUM("중간", 0.5),
+    WEAK("약함", 0.0);
 
     private final String label;
+    private final Double weight;
 
-    Degree(String label) {
+    Degree(String label, Double weight) {
         this.label = label;
+        this.weight = weight;
     }
 
     public String getLabel() {
         return label;
+    }
+
+    public Double getWeight() {
+        return weight;
     }
 
     private static final Map<String, Degree> BY_LABEL =
@@ -25,5 +31,12 @@ public enum Degree {
 
     public static Degree valueOfLabel(String label) {
         return BY_LABEL.get(label);
+    }
+
+    public static final Map<Double, Degree> BY_WEIGHT =
+            Stream.of(values()).collect(Collectors.toMap(Degree::getWeight, e -> e));
+
+    public static Degree valueOfWeight(Double weight) {
+        return BY_WEIGHT.get(weight);
     }
 }
