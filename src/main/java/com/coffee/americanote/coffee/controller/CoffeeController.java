c2a9e0c0224d085e9bed1e848ca9deb4171fa4d5
dabeann <dabin4078@daum.net>
2024-03-29 15:42:09 +0900

    style: remove swagger /api/coffee/all

--- src/main/java/com/coffee/americanote/coffee/controller/CoffeeController.java
+++ src/main/java/com/coffee/americanote/coffee/controller/CoffeeController.java
@@ -2,12 +2,7 @@ package com.coffee.americanote.coffee.controller;
 
 import com.coffee.americanote.coffee.domain.response.CoffeeResponse;
 import com.coffee.americanote.coffee.service.CoffeeService;
-import com.coffee.americanote.common.response.BasicApiSwaggerResponse;
 import com.coffee.americanote.common.response.CommonResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Coffee", description = "커피 관련 API입니다.")
 @RequestMapping("/api/coffee")
 @RequiredArgsConstructor
 @RestController
@@ -24,9 +18,7 @@ public class CoffeeController {
 
     private final CoffeeService coffeeService;
 
-    @Operation(summary = "summary : 모든 커피 조회", description = "description : return all coffee list")
-    @BasicApiSwaggerResponse
-    @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json"))
+    // test api
     @GetMapping("/all")
     ResponseEntity<CommonResponse<List<CoffeeResponse>>> getAllCoffee() {
         return new ResponseEntity<>(new CommonResponse<>("모든 커피 조회", coffeeService.getAllCoffeeData()), HttpStatus.OK);
