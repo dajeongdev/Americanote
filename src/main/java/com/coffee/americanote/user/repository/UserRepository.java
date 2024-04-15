376679919484521079ca0e8064201aab8b1953b1
dajeongdev <dajeongdev@gmail.com>
2024-04-16 00:10:45 +0900

    fix : querydsl 추가로 사용하지 않는 쿼리메소드 삭제

--- src/main/java/com/coffee/americanote/user/repository/UserRepository.java
+++ src/main/java/com/coffee/americanote/user/repository/UserRepository.java
@@ -2,14 +2,12 @@ package com.coffee.americanote.user.repository;
 
 import com.coffee.americanote.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
-    Optional<User> findByKakaoId(Long kakaoId);
+public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.flavours WHERE u.id = :userId")
     Optional<User> findByIdWithFlavours(@Param("userId") Long userId);
